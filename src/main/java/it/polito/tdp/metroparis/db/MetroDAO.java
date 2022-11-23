b929ce5a12c6b59eb3e49747365490054516f739
HP <HP@172.18.94.123>
2022-11-23 18:10:33 +0100

    creazione grafo

--- src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
+++ src/main/java/it/polito/tdp/metroparis/db/MetroDAO.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 import com.javadocmd.simplelatlng.LatLng;
 
-import it.polito.tdp.metroparis.model.Connessione;
+import it.polito.tdp.metroparis.model.CoppiaId;
 import it.polito.tdp.metroparis.model.Fermata;
 import it.polito.tdp.metroparis.model.Linea;
 
@@ -43,7 +43,8 @@ public class MetroDAO {
 	}
 
 	public List<Linea> getAllLinee() {
-		final String sql = "SELECT id_linea, nome, velocita, intervallo FROM linea ORDER BY nome ASC";
+		final String sql = "SELECT id_linea, nome, velocita, intervallo FROM linea"+
+	            " ORDER BY nome ASC";
 
 		List<Linea> linee = new ArrayList<Linea>();
 
@@ -67,6 +68,123 @@ public class MetroDAO {
 		}
 
 		return linee;
+	}
+
+	
+//METODO 1
+	public boolean isFermateConnesse(Fermata partenza,Fermata arrivo) {
+
+		final String sql = "SELECT * FROM connessione"
+				+ " WHERE id_stazP=? AND id_stazA=?";
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			st.setInt(2, arrivo.getIdFermata());
+			ResultSet rs = st.executeQuery();
+			rs.first();
+			int count= rs.getInt("cnt");			
+			
+			st.close();
+			conn.close();
+			
+			return count>0;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore Database.",e);
+			
+		}
+	}
+
+	
+//METODO 2
+	public List<Integer> getIdFermateConnesse (Fermata partenza) {
+
+		final String sql = "SELECT id_stazA FROM connessione WHERE id_stazP=? GROUP BY id_stazA";
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			ResultSet rs = st.executeQuery();
+			
+			List<Integer> result= new ArrayList<>();
+			
+			while(rs.next()) {
+				result.add(rs.getInt("id_stazA"));
+			}
+			
+			st.close();
+			conn.close();
+			
+			return result;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+			
+		}
+	}
+
+	
+//METODO 2b
+	public List<Fermata> getFermateConnesse(Fermata partenza) {
+
+		final String sql = "SELECT id_fermata, nome, coordx, coordy FROM fermata"+
+		        " WHERE id_fermata IN(SELECT id_stazA FROM connessione WHERE id_stazP=? GROUP BY id_stazA)"+
+				" ORDER BY nome ASC";
+		List<Fermata> fermate = new ArrayList<Fermata>();
+
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, partenza.getIdFermata());
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+				Fermata f = new Fermata(rs.getInt("id_Fermata"), rs.getString("nome"),
+						new LatLng(rs.getDouble("coordx"), rs.getDouble("coordy")));
+				fermate.add(f);
+			}
+
+			st.close();
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore Database.");
+		}
+
+		return fermate;
+	}
+
+	
+//METODO 3
+	public List<CoppiaId> getCoppiaFermateConnesse(){
+		final String sql = "SELECT Distinct id_stazP, id_stazA FROM connessione";
+		List<CoppiaId> coppiaId = new ArrayList<>();
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+
+			while (rs.next()) {
+				CoppiaId cId = new CoppiaId(rs.getInt("id_stazP"), rs.getInt("id_stazA"));
+				coppiaId.add(cId);
+			}
+
+			st.close();
+			conn.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Errore Database.");
+		}
+
+		return coppiaId;
 	}
 
 	
---
+++ src/main/java/it/polito/tdp/metroparis/model/Model.java
@@ -0,0 +1,89 @@
+package it.polito.tdp.metroparis.model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleDirectedGraph;
+
+import it.polito.tdp.metroparis.db.MetroDAO;
+
+public class Model {
+
+	private Graph <Fermata, DefaultEdge> grafo;
+	
+	
+	public void creaGrafo() {
+		this.grafo = new SimpleDirectedGraph<Fermata, DefaultEdge>(DefaultEdge.class);
+		
+		
+	    //creo VERTICI
+		MetroDAO dao= new MetroDAO();
+		List<Fermata> fermate =dao.getAllFermate();
+		Map<Integer, Fermata> fermateIdMap= new HashMap<Integer,Fermata>();
+		for(Fermata f: fermate) {
+			fermateIdMap.put(f.getIdFermata(), f);
+		}
+		
+		//this.grafo.addVertex(fermata);
+		Graphs.addAllVertices(this.grafo, fermate);
+		System.out.println(this.grafo);
+		
+	//creo ARCHI
+		//METODO 1 : itero su ogni coppia di vertici, metodo lungo
+		/*for(Fermata partenza: fermate) {
+			for(Fermata arrivo: fermate ) {
+				if(dao.isFermateConnesse(partenza, arrivo)){  //esiste almeno una connessione
+			    	this.grafo.addEdge(partenza, arrivo);
+		    	}
+			}
+		}*/
+		
+		//METODO2: per ogni vertice , cerco i vertici ad esso adiacenti
+		/*for( Fermata partenza: fermate) {
+			List<Integer> idConnesse= dao.getIdFermateConnesse(partenza);
+			for(Integer id: idConnesse) {
+				Fermata arrivo =null;
+				for(Fermata f:fermate) {
+					if(f.getIdFermata()==id){
+						arrivo=f; //fermata che possiede quell' id
+						break;
+					}
+				}
+				this.grafo.addEdge(partenza, arrivo);
+			}
+		}*/
+		
+		//METODO 2b: il DAO restituisce un elenco di oggetti fermata
+		/*for(Fermata partenza:fermate) {
+			List<Fermata> arrivi= dao.getFermateConnesse(partenza);
+			for(Fermata arrivo :arrivi) {
+				this.grafo.addEdge(partenza, arrivo);
+			}
+		  }*/
+		
+		//METODO 2c [IDENTITY MAP]: il DAO restituisce un elenco di id numerici,
+		//che converto in oggetti tramite una Map<Integer,Fermata (preferita)
+		/*for(Fermata partenza:fermate) {
+			List<Integer> idConnesse= dao.getIdFermateConnesse(partenza);
+			for(int id : idConnesse) {
+				Fermata arrivo= fermateIdMap.get(id);
+				this.grafo.addEdge(partenza, arrivo);
+			}
+		}*/
+		
+		//METODO 3 [delego al database]: faccio una sola query che restituisca la coppia di fermate da collegare
+		List<CoppiaId> fermateDaCollegare=dao.getCoppiaFermateConnesse();
+		for(CoppiaId coppia: fermateDaCollegare) {
+			this.grafo.addEdge(fermateIdMap.get(coppia.idPartenza), fermateIdMap.get(coppia.idArrivo));
+		}
+		
+		
+		System.out.println("Vertici: "+this.grafo.vertexSet().size());
+		
+		System.out.println("Archi: "+this.grafo.edgeSet().size());
+	}
+}
